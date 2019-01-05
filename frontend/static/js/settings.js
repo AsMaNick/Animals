e63eb839c046b27b3ed6394fe1f483b2169e14de
DE0414\User <aslandukov-matvej@yandex.ua>
2019-01-05 18:45:19 +0200

    Redesigned start page

---
+++ frontend/static/js/settings.js
@@ -0,0 +1,37 @@
+﻿function setLanguageFlag(language) {
+	document.getElementById('language_flag').src = './static/img/' + language + '-flag.png';
+}
+
+function initLanguage() {
+	if (localStorage.getItem('language') === null) {
+		localStorage['language'] = 'en';
+	}
+	setLanguageFlag(localStorage['language']);
+}
+
+initLanguage();
+
+function getLanguageId(language) {
+	if (language == 'en') {
+		return 0;
+	}
+	return 1;
+}
+
+function changeLanguage(language) {
+	setLanguageFlag(language);
+	localStorage['language'] = language;
+	header_nav_bar_app.language_id = getLanguageId(language);
+}
+
+var header_nav_bar_app = new Vue({
+	el: '#header_nav_bar',
+	data: {
+		language_id: getLanguageId(localStorage['language']),
+		literals: {
+			languages: ['english', 'українська'],
+			sign_up: ['Sign Up', 'Зареєструватися'],
+			login: ['Login', 'Увійти'],
+		}
+	}
+});

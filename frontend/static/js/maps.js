9c8da5542d49d2e8e9b2100b925b06cb476775c3
DE0414\User <aslandukov-matvej@yandex.ua>
2019-01-15 11:02:35 +0200

    Added me label

--- frontend/static/js/maps.js
+++ frontend/static/js/maps.js
@@ -23,13 +23,13 @@ function initMap() {
 		.then(response => {
 			for (var user of response.data) {
 				if (user.lat !== null && user.lng != null) {
-					console.log(user);
 					var marker = new google.maps.Marker({
 						position: {
 							lat: user.lat,
 							lng: user.lng
 						}, 
-						map: map
+						map: map,
+						label: ['', 'M'][(user.id == getClient().id) ? 1 : 0]
 					});
 					var contentString = '<a href="./viewuser.html?{0}"> <img src="{1}{2}" class="img-circle pet_small_photo"> </a>'.format(user.id, vue_app.DOMAIN, user.avatar) +
 						'<a href="./viewuser.html?{0}"> {1} {2} </a>'.format(user.id, user.name, user.surname);
